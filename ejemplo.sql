4774b876da7a64285acc924f08c99578b0f50969
Joel Leon M <109642410+JoelLeonUNS@users.noreply.github.com>
2025-06-11 09:48:00 -0500

    Update ejemplo.sql

--- ejemplo.sql
+++ ejemplo.sql
@@ -5,8 +5,8 @@ DROP TABLE IF EXISTS cliente;
 CREATE TABLE cliente 
  (id                         VARCHAR(3)  NOT NULL,
   nombre                     VARCHAR(50) NOT NULL,
-  telefono                   VARCHAR(50) NOT NULL,
-  direccion                  VARCHAR(50),
+  telefono                   VARCHAR(20) NOT NULL,
+  direccion                  VARCHAR(20),
   ciudad                     VARCHAR(20),
   provincia                  VARCHAR(15),
   pais                       VARCHAR(20),
